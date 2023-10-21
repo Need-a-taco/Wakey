b1300414066bc4586714e3f891abeec4bdf47c9e
Emily Luo <emilyluo555@gmail.com>
2023-10-21 03:22:13 -0400

    soundboard

---
+++ Wakey-app/components/soundboard/soundboard.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  useWindowDimensions,
+  Image,
+  TextInput,
+} from "react-native";
+import SoundButton from "./soundButtons";
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+  },
+  smallText: {
+    color: "white",
+    fontSize: 20,
+  },
+  timeText: {
+    color: "white",
+    fontSize: 15,
+    fontStyle: "italic",
+    fontWeight: "bold",
+  },
+  bigText: {
+    color: "white",
+    fontSize: 55,
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    margin: 10,
+  },
+});
+
+const SoundBoard = () => {
+  return (
+    <View style={styles.container}>
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Text style={styles.timeText}>It's time to</Text>
+        <Text style={styles.bigText}>Wake Up</Text>
+      </View>
+      <View>
+        <Text style={styles.smallText}>Friend 1</Text>
+      </View>
+      <View>
+        <View style={styles.buttonContainer}>
+          <SoundButton buttonType={"trumpet"} />
+          <SoundButton buttonType={"siren"} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <SoundButton buttonType={"bruh"} />
+          <SoundButton buttonType={"fart"} />
+        </View>
+      </View>
+    </View>
+  );
+};
+export default SoundBoard;
