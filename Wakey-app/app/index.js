61937df0ef9b8d5c1c69bc7f3a1c2b8de2866042
Joshua Zhang <112833917+Need-a-taco@users.noreply.github.com>
2023-10-20 22:51:16 -0400

    Update index.js

--- Wakey-app/app/index.js
+++ Wakey-app/app/index.js
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
+import Clock from "../components/homepage/clock";
+
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>
+        Goodnight!
+      </Text>
+      <Text>
+        bruh
+      </Text>
+      <Text>Peepee wee</Text>
     </View>
   );
 };
