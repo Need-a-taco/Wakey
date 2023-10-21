7fe715fc515755feb8dd9417e91405abff511ca4
Eric <eric3691215@gmail.com>
2023-10-20 23:52:05 -0400

    added set alarm button

---
+++ Wakey-app/components/homepage/timePicker.js
@@ -0,0 +1,31 @@
+import { View, Text } from "react-native";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { useState } from "react";
+import { SafeAreaView } from 'react-native';
+
+
+const TimePicker = () => {
+  const [date, setDate] = useState(new Date());
+
+  const changedTime = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setDate(currentDate);
+  };
+
+  return (
+    <SafeAreaView>
+      {/* <Text>selected: {date.toLocaleString()}</Text> */}
+      <DateTimePicker
+        testID="dateTimePicker"
+        value={date}
+        mode={'time'}
+        display={'spinner'}
+        is24Hour={true}
+        onChange={changedTime}
+        themeVariant="dark"
+      />
+    </SafeAreaView>
+  );
+};
+
+export default TimePicker;
