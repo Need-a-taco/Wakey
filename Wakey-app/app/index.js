dd53b0c773f78f79dcc9e06c471273def9605309
Eric <eric3691215@gmail.com>
2023-10-21 02:43:22 -0400

    pre merge

--- Wakey-app/app/index.js
+++ Wakey-app/app/index.js
@@ -1,13 +1,14 @@
 import { View, Text, SafeAreaView } from "react-native";
 import Clock from "../components/homepage/clock";
-import { Stack, useRouter } from "expo-router";
+// import { Stack, useRouter } from "expo-router";
 import InputField from "../components/homepage/inputField";
 import TimePicker from "../components/homepage/timePicker";
 import SetAlarmBtn from "../components/homepage/setAlarmBtn";
+import { Stack } from "expo-router";
 
-const Home = () => {
+function Home () {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#1d1e1f" }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#1d1e1f" }}>
       <Stack.Screen
         options={{
           headerStyle: { backgroundColor: "#1d1e1f" },
