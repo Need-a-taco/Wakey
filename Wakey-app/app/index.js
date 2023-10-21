085f9ca09d4b0a976ea88259e830787e32cbb623
Eric <eric3691215@gmail.com>
2023-10-20 22:22:50 -0400

    time picker basic

--- Wakey-app/app/index.js
+++ Wakey-app/app/index.js
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
+import TimePicker from "../components/homepage/setAlarm.js";
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
+      <TimePicker />
     </View>
   );
 };
